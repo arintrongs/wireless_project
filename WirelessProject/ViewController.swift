daa50805dba780a6a97217fac3009c1ef5d280e3
Arin Trongsantipong <arintrongs@gmail.com>
2019-11-07 18:29:22 +0700

    Initial Commit

---
+++ WirelessProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WirelessProject
+//
+//  Created by TomFoolery on 7/11/2562 BE.
+//  Copyright © 2562 wireless_project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
